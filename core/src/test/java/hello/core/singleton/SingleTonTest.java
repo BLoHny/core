5574a5d3be2e899ebbb3c9e749158c391ff6d042
BLoHny <113517233+BLoHny@users.noreply.github.com>
2023-03-20 23:49:27 +0900

    d

---
+++ core/src/test/java/hello/core/singleton/SingleTonTest.java
@@ -0,0 +1,39 @@
+package hello.core.singleton;
+
+import hello.core.AppConfig;
+import hello.core.member.MemberService;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+
+public class SingleTonTest {
+
+    @Test
+    @DisplayName("스프링 없는 순수한 DI 컨테이너")
+    void pureContainer() {
+        AppConfig appConfig = new AppConfig();
+        //1. 호출할때마다 객체 생성
+        MemberService memberService = appConfig.memberService();
+
+        //2. ~~ ""
+        MemberService memberService1 = appConfig.memberService();
+
+        System.out.println("memberService = " + memberService);
+        System.out.println("memberService1 = " + memberService1);
+
+        assertThat(memberService).isNotSameAs(memberService1);
+    }
+
+    @Test
+    @DisplayName("싱글톤 패턴을 적용한 객체 사용")
+    void singletonServiceTest() {
+        SingletonService instance = SingletonService.getInstance();
+        SingletonService instance2 = SingletonService.getInstance();
+
+        assertThat(instance).isSameAs(instance2);
+        //sameAs -> ==
+        //equal -> 객체 비
+    }
+}
